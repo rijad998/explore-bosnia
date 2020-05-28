417b753c1f09b4ea192092509ca7db192b5222a9
rijad998 <rijad.babovic@hotmail.com>
2020-05-29 01:29:58 +0200

    entry page UI

--- app/src/main/java/com/example/sparklingsmiledentalclinic/MainActivity.java
+++ app/src/main/java/com/example/sparklingsmiledentalclinic/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.sparklingsmiledentalclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerBtn = findViewById(R.id.registerBtn);
+        loginBtn = findViewById(R.id.loginBtn);
     }
 }
