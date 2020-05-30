39f81f353b41563fc747ea033a996a1e07036a91
rijad998 <rijad.babovic@hotmail.com>
2020-05-30 20:14:45 +0200

    adding registration with firebase authentication and custom register fields in firebase realtime database

--- app/src/main/java/com/example/sparklingsmiledentalclinic/RegisterActivity.java
+++ app/src/main/java/com/example/sparklingsmiledentalclinic/RegisterActivity.java
@@ -1,28 +1,116 @@
 package com.example.sparklingsmiledentalclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText phone;
-    EditText email;
-    EditText password;
+    EditText nameEditText;
+    EditText phoneEditText;
+    EditText emailEditText;
+    EditText passwordEditText;
     Button registerProceed;
+    ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        name = findViewById(R.id.nameEditText);
-        phone = findViewById(R.id.phoneEditText);
-        email = findViewById(R.id.emailEditText);
-        password = findViewById(R.id.passwordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
         registerProceed = findViewById(R.id.registerProceedBtn);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        try {
+            registerProceed.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    registerUser();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void registerUser() {
+        final String name = nameEditText.getText().toString().trim();
+        final String phone = phoneEditText.getText().toString().trim();
+        final String email = emailEditText.getText().toString().trim();
+        final String password = passwordEditText.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            nameEditText.setError("Enter a valid name.");
+            nameEditText.requestFocus();
+            return;
+        }
+
+        if (phone.isEmpty() || phone.length() < 9) {
+            phoneEditText.setError("Enter a valid phone number.");
+            phoneEditText.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Enter a valid e-mail address.");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            passwordEditText.setError("Password must be at least 6 characters.");
+            passwordEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    User user = new User(name, email, phone);
+
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Adding additional data besides email and password failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Registration Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
